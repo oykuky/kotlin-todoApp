b3fc871a41f84d370fb047097702c761e7db98b1
Öykü Kaya <oykuky001@gmail.com>
2024-04-03 18:27:37 +0300

    Initial commit

---
+++ app/src/main/java/com/example/kotlin_todoapp/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.kotlin_todoapp
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+
+class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: TodoAdapter
+    private val todoList = mutableListOf<String>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val addButton: Button = findViewById(R.id.addButton)
+        val removeAllButton: Button = findViewById(R.id.removeAllButton)
+        val editText: EditText = findViewById(R.id.editText)
+
+        recyclerView = findViewById(R.id.recyclerView)
+        adapter = TodoAdapter(todoList)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        addButton.setOnClickListener {
+            val newTodo = editText.text.toString()
+            if (newTodo.isNotBlank()) {
+                todoList.add(newTodo)
+                adapter.notifyDataSetChanged()
+                editText.text.clear()
+            }
+        }
+
+        removeAllButton.setOnClickListener {
+            todoList.clear()
+            adapter.notifyDataSetChanged()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/kotlin_todoapp/TodoAdapter.kt
@@ -0,0 +1,28 @@
+package com.example.kotlin_todoapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class TodoAdapter(private val todoList: List<String>) :
+    RecyclerView.Adapter<TodoAdapter.ViewHolder>() {
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val textView: TextView = itemView.findViewById(android.R.id.text1)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(android.R.layout.simple_list_item_1, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val todo = todoList[position]
+        holder.textView.text = todo
+    }
+
+    override fun getItemCount(): Int = todoList.size
+}
